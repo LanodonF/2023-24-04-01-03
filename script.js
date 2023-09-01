0a02959034b016296af3e2667980972aff0c14ba
LanodonF <122838482+LanodonF@users.noreply.github.com>
2023-09-01 13:02:16 +0000

    Refactored and working HTML and CSS. Added Favicon.

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
-let
-let score = 0; 
-let scorebruh= document.getElementById('score')
-const mines = document.getElementsByClassName('plot')
-let bruh = document.getElementById('garden')
+//Math.sqrt(document.getElementById("main").querySelectorAll("span").length)
 
+let score = 0; 
+let scorebruh= document.getElementById('score');
+const mines = document.getElementsByClassName('plot');
+const main = document.getElementById('main')
+const rows = document.getElementsByClassName('row');
 if (localStorage.plays == undefined){
-  localStorage.setItem("plays", "1")
+  localStorage.setItem("plays", "1");
 }
 else{
   addEventListener('load',()=>{localStorage.setItem("plays", 
-    parseInt(localStorage.getItem("plays"))+1)
+    parseInt(localStorage.getItem("plays"))+1);
 })}
+
+if (localStorage.level == undefined){
+  localStorage.setItem("level","5");
+}
+
+addEventListener('load',()=>{
+  for (let i =0;  i < localStorage.level; i++){
+    main.appendChild(document.createElement('div'));
+    
+    //console.log("Bruh");
+  }
+  let divvs = document.getElementsByTagName('div');
+  for (let i =0;  i < localStorage.level; i++){
+    for (let div of divvs){
+       div.className = "row";
+       div.appendChild(document.createElement("span"));
+  }
+}
+  let spans = document.getElementsByTagName("span")
+  for(let span of spans){
+    span.className= "plot"
+    span.innerHTML="\"<i class=\"fa-solid fa-check\"></i>\""
+  }
+}
+)
+
 
 for (const mine of mines) {
   mine.addEventListener('click', ()=>{
